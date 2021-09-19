416abe12d2873c86f207b0aa50c45b623d828487
djtran <dtran745@gmail.com>
2021-09-19 00:40:30 -0400

    Timer display & buttons
    Timer initialized at the bottom of the code editor. Timer can be started and stopped manually.

--- src/main.js
+++ src/main.js
@@ -8,8 +8,16 @@ const DIFFICULTY = {
     UNKNOWN: "Unknown"
 }
 
+function getProblemURL() {
+    return window.location.href;
+}
+
+//null or a text value
+function getProblemName() {
+    return document.querySelector('[data-cy="question-title"]').innerText;
+}
+
 function getDifficulty() {
-    
     if (document.querySelectorAll('[diff="easy"]').length == 1) {
         return DIFFICULTY.EASY;
     } else if (document.querySelectorAll('[diff="medium"]').length == 1) {
@@ -21,17 +29,88 @@ function getDifficulty() {
     }
 }
 
+const submissionResultElement = "[class*='result__']";
+const submissionSuccessElement = "[class*='success__']";
+const submissionErrorElement = "[class*='error__']";
+
+function waitForSubmitResponse() {
+    let success = document.querySelector("[class*='result__']").querySelector("[class*='success__']").innerText;
+    
+    //WrongAnswer, Time Limit Exceeded, Compile Error
+    let error = document.querySelector("[class*='result__']").querySelector("[class*='error__']").innerText;
+
+    if (success != null) {
+        //stop the problem timer, save the problem data.
+        clearInterval(submitTimer);
+    } else if (error != null) {
+        //increment the error counter for this problem.
+        clearInterval(submitTimer);
+    }
+    //else we poll again some time later.
+
+
+}
+
+var submitTimer;
+
+function addClickListenerToSubmitButton() {
+    document.querySelector('[data-cy="submit-code-btn"]').onclick = function checkSubmission() {
+        setInterval(()=>{
+            
+        }, 250)
+    }
+}
+var currentProblem = "";
+
+function detectChanges() {
+    let title = getProblemName();
+    let difficulty = getDifficulty();
+    let url = getProblemURL();
+
+    if(title != currentProblem) {
+        currentProblem = title;
+        console.log("new problem, adding timer for " + title);
+        
+        //init timer
+        let referenceNode = document.querySelector("[data-cy='run-code-btn']");
+        timerDisplay = document.createElement("label");
+        timerDisplay.classList.add('timer');
+        timerDisplay.innerText = "00:00:00";
+        timerDisplay.style.marginRight = "16px";
+        
+        //start button
+        startTimerButton = document.createElement("button");
+        startTimerButton.classList.add('startTimer')
+        startTimerButton.innerText = "Start Timer"
+        startTimerButton.style.marginRight = "16px";
+        startTimerButton.onclick = startTimer;
+        
+        //pause button
+        pauseTimerButton = document.createElement("button");
+        pauseTimerButton.classList.add('pauseTimer')
+        pauseTimerButton.innerText = "Pause Timer"
+        pauseTimerButton.style.marginRight = "16px";
+        pauseTimerButton.onclick = pauseTimer;
+
+        referenceNode.parentNode.insertBefore(startTimerButton, referenceNode);
+        referenceNode.parentNode.insertBefore(pauseTimerButton, referenceNode);
+        referenceNode.parentNode.insertBefore(timerDisplay, referenceNode);
+
+        //init code editor listener to start timer automatically if paused but typing.
+    }
+
+
+}
+
+var currentProblem = null;
 // Main onLoadPage function, starts the cycles needed to discover the elements inside the page
 // and to attach listeners to them
 function onLoadPage (evt) {
-    // insert the styles for the custom components
-    // var styleSheet = document.createElement("style")
-    // styleSheet.type = "text/css"
-    // styleSheet.innerText = timerStyle
-    // document.head.appendChild(styleSheet)
 
-    // setInterval(checkForMutations, 500);
-    console.log("Difficulty: " + getDifficulty());
+    setInterval(detectChanges,500);
+    console.log("page loaded");
+
+
 
 
 }
---
+++ src/timer.js
@@ -0,0 +1,76 @@
+/**
+ * Timer implementation provided from https://olinations.medium.com/an-accurate-vanilla-js-stopwatch-script-56ceb5c6f45b, which I found through the LeetPlug project.
+ */
+
+var startTimerButton = document.querySelector('.startTimer');
+var pauseTimerButton = document.querySelector('.pauseTimer');
+var timerDisplay = document.querySelector('.timer');
+var startTime;
+var updatedTime;
+var difference;
+var tInterval;
+var savedTime;
+var paused = 0;
+var running = 0;
+function startTimer() {
+    if (!running) {
+        startTime = new Date().getTime();
+        tInterval = setInterval(getShowTime, 1000);
+        // change 1 to 1000 above to run script every second instead of every millisecond. one other change will be needed in the getShowTime() function below for this to work. see comment there.   
+
+        paused = 0;
+        running = 1;
+        startTimerButton.classList.add('lighter');
+        pauseTimerButton.classList.remove('lighter');
+        startTimerButton.style.cursor = "auto";
+        pauseTimerButton.style.cursor = "pointer";
+    }
+}
+function pauseTimer() {
+    if (!difference) {
+        // if timer never started, don't allow pause button to do anything
+    } else if (!paused) {
+        clearInterval(tInterval);
+        savedTime = difference;
+        paused = 1;
+        running = 0;
+        startTimerButton.classList.remove('lighter');
+        pauseTimerButton.classList.add('lighter');
+        startTimerButton.style.cursor = "pointer";
+        pauseTimerButton.style.cursor = "auto";
+    } else {
+        // if the timer was already paused, when they click pause again, start the timer again
+        startTimer();
+    }
+}
+function resetTimer() {
+    clearInterval(tInterval);
+    savedTime = 0;
+    difference = 0;
+    paused = 0;
+    running = 0;
+    timerDisplay.innerHTML = 'Start Timer!';
+    timerDisplay.style.background = "#A90000";
+    timerDisplay.style.color = "#fff";
+    timerDisplay.style.cursor = "pointer";
+    startTimerButton.classList.remove('lighter');
+    pauseTimerButton.classList.remove('lighter');
+    startTimerButton.style.cursor = "pointer";
+    pauseTimerButton.style.cursor = "auto";
+}
+function getShowTime() {
+    updatedTime = new Date().getTime();
+    if (savedTime) {
+        difference = (updatedTime - startTime) + savedTime;
+    } else {
+        difference = updatedTime - startTime;
+    }
+    // var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds;
+}
